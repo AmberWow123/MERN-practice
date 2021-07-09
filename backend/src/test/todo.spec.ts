a573a410249b98a06cb9cc0b285ac8c0916d5d92
Yi Ting Wang <y9wang@ucsd.edu>
2021-07-09 08:57:40 +0000

    complete backend and frontend

--- backend/src/test/todo.spec.ts
+++ backend/src/test/todo.spec.ts
@@ -34,4 +34,42 @@ describe('Form test', () => {
     })    
 
     // TODO: Add some test cases like CRUD, i.e. get, post, update, delete
+    it('should get todo item after input', async () => {
+
+
+        const response = await server.inject({
+            method:'POST',
+            url: '/api/todos',
+            payload: {
+                name: 'clean my desk',
+                description: 'should be done before lunch',
+                status: false
+            }
+        })
+
+        expect(response.statusCode).toBe(201)
+        const res: {todo: ITodo } = JSON.parse(response.body)
+        console.log(`post Todo: ${response.body}`)
+        expect(res.todo.name).toBe('clean my desk')
+        expect(res.todo.description).toBe('should be done before lunch')
+        expect(res.todo.status).toBe(false)
+
+
+        // test if add successfully with get
+        const getResponse = await server.inject({
+            method: 'GET',
+            url: '/api/todos'
+        })
+        expect(getResponse.statusCode).toBe(200)
+        const res2: { todos: Array<ITodo> } = JSON.parse(getResponse.body)
+        console.log(`get Todos: ${getResponse.body}`)
+        expect(res2.todos.length).toBe(1)
+        expect(res2.todos[0].name).toBe('clean my desk')
+        expect(res2.todos[0].description).toBe('should be done before lunch')
+        expect(res2.todos[0].status).toBe(false)
+    })
+
+
+
+    
 })
